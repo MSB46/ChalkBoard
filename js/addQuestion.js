a6429680ae08e569e31f3bb84dfe900c031a6764
Michael <33902649+MSB46@users.noreply.github.com>
2021-12-12 20:55:39 -0500

    Add files via upload

---
+++ js/addQuestion.js
@@ -0,0 +1,28 @@
+let counter = 1;
+function addQuestion(){
+    let error = document.querySelector(".error");
+    let questions = document.querySelector(".questions");
+    if(counter >= 5){
+        error.classList.remove("hide");
+        return;
+    }
+
+    counter++;
+    questions.innerHTML += (`
+        <label for="desc-input${counter}" class="asking-for" id="q${counter}">Question ${counter}</label> <br>
+        <textarea class="cred-input" placeholder="Question ${counter}" name="desc-input${counter}" id="desc-input${counter}" rows="5" cols="80" required> </textarea>`);
+}
+
+function removeQuestion(){
+    let error = document.querySelector(".error");
+    if(counter <= 1){
+        return;
+    }
+    error.classList.add("hide");
+    let toBeRemoved = document.getElementById(`q${counter}`);
+    toBeRemoved.remove();
+
+    let toBeRemoved2 = document.getElementById(`desc-input${counter}`);
+    toBeRemoved2.remove();
+    counter--;
+}
